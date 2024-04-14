d9d54fa125d6be607976ff304ddc671005495ef7
Gvantsie <g_euashvili@cu.edu.ge>
2024-04-15 00:18:51 +0400

    Updated media folder endpoints

--- market/models.py
+++ market/models.py
@@ -12,7 +12,7 @@ class Book(models.Model):
     category = models.CharField(verbose_name=_("Category"), max_length=50)
     price = models.DecimalField(verbose_name=_("Price"), max_digits=5, decimal_places=2)
     stock = models.IntegerField(verbose_name=_("Stock"))
-    cover = models.ImageField(verbose_name=_("Cover"), upload_to='media/covers/', null=True, blank=True)
+    cover = models.ImageField(verbose_name=_("Cover"), upload_to='covers/', null=True, blank=True)
 
     def __str__(self):
         return self.title
