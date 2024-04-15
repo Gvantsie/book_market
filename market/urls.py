20c5342f8e35c5011792a4613f543b76f9e0bf94
Gvantsie <g_euashvili@cu.edu.ge>
2024-04-15 04:34:33 +0400

    Updated urls.py

--- market/urls.py
+++ market/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 from django.contrib import admin
-from .views import home
+from .views import home, filtered_books
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
     path('books/', views.get_books, name='get_books'),
     path('book_detail/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('filtered_books/', filtered_books, name='filtered_books'),
 ]
