5dde6ef0c90da9717acb00fca14f4ad328669e81
Gvantsie <g_euashvili@cu.edu.ge>
2024-04-15 18:02:02 +0400

    initial commit

--- book_market/settings.py
+++ book_market/settings.py
@@ -125,15 +125,12 @@ charset: str = 'utf-8'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static_common/'
-STATIC_ROOT = BASE_DIR / 'static_common'
+STATIC_URL = 'static_files/'
+STATIC_ROOT = BASE_DIR / 'static_files_root'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'media',
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
