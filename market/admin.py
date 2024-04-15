d811845916322553432505f1f075858fcc04698f
Gvantsie <g_euashvili@cu.edu.ge>
2024-04-15 04:31:39 +0400

    Updated admin.py

--- market/admin.py
+++ market/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
 from market.models import Book
+from market.models import Author
+from market.models import Category
+
 
 # Register your models here.
-# admin.site.register(Book)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    list_filter = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    search_fields = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name',)
+    list_filter = ('category_name',)
+    search_fields = ('category_name',)
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'price', 'stock')
-    list_filter = ('title', 'author', 'best_seller', 'category')
-    search_fields = ('title', 'author', 'price', 'category')
+    list_filter = ('title', 'author', 'best_seller', 'categories')
+    search_fields = ('title', 'author', 'price', 'categories')
 
-
-def site(request):
-    return None
