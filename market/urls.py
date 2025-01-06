5490c2666dfdc5a17d88fa373eec8098b1c52af9
Gvantsie <g_euashvili@cu.edu.ge>
2025-01-07 02:47:58 +0400

    book_detail.html modifications

--- market/urls.py
+++ market/urls.py
@@ -6,7 +6,7 @@ from .views import filtered_books, filtered_authors
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.get_books, name='books'),
+    path('', views.get_books, name='get_books'),
     path('book_detail/<int:book_id>/', views.book_detail, name='book_detail'),
     path('filtered_books/', filtered_books, name='filtered_books'),
     path('filtered_authors/', filtered_authors, name='filtered_authors'),
